45586fcec2464854ad211c23af08909e60b67865
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-30 20:35:03 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,6 @@ import java.util.Arrays;
 
 public class Main {
 
-    // Class is a template and doesn't occupy space in the memory
-    // Class is a named group of properties and functions
-
-
     public static void main(String[] args) {
 
         // Store 5 roll no's
@@ -18,7 +14,11 @@ public class Main {
         // Current value is null
         // new allocates memory at run time
         // dynamically means memory is allocated at run time
-        Student harsh = new Student(12,"Harsh",93);
+        Student temp = new Student();
+        Student harsh = new Student(12,"Harsh",93.2F);
+        Student random = new Student(harsh);
+
+        System.out.println(harsh.rno);
 
     }
 }
