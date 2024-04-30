dd5de7386dae9322ed8a01fdcc2e123b258bbb69
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-30 20:11:40 +0530

    added

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,7 @@ public class Main {
 
     // Class is a template and doesn't occupy space in the memory
     // Class is a named group of properties and functions
-    class Student{
 
-        // These are called instance variables
-        int rno;
-        String names;
-        float marks;
-
-        // Constructor is
-    }
 
     public static void main(String[] args) {
 
@@ -26,7 +18,7 @@ public class Main {
         // Current value is null
         // new allocates memory at run time
         // dynamically means memory is allocated at run time
-        Student[] students = new Student[5];
-        System.out.println(Arrays.toString(students));
+        Student harsh = new Student(12,"Harsh",93);
+
     }
 }
---
+++ src/Student.java
@@ -0,0 +1,14 @@
+class Student{
+
+    // These are called instance variables
+    int rno;
+    String name;
+    float marks;
+
+    // Constructor is a special function which runs when an object is created
+    Student(int rno, String name, float marks){
+        this.rno = rno;
+        this.name = name;
+        this.marks = marks;
+    }
+}
