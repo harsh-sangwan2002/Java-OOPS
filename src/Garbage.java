34a26ed3344535f9a731f91dfa964e30e03dd979
harsh-sangwan2003 <hsangwan2002@gmail.com>
2024-04-30 20:52:12 +0530

    added

---
+++ src/Garbage.java
@@ -0,0 +1,19 @@
+public class Garbage {
+
+    Garbage(){
+        System.out.println("Object created");
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        System.out.println("Object destroyed");
+    }
+
+    public static void main(String[] args) {
+
+        Garbage g;
+
+        for(int i=0; i<1000000; i++)
+            g = new Garbage();
+    }
+}
